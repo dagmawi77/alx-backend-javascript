9be1910ca7ea913eaa95e1480c20848fe87d1c02
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-10-03 12:31:57 +0300

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,16 +1,9 @@
 export default function groceriesList() {
-  const arr = [
+  return new Map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
-  ];
-
-  const mp = new Map();
-  // eslint-disable-next-line no-unused-vars
-  for (const el of arr) {
-    mp.set(el[0], el[1]);
-  }
-  return mp;
+  ]);
 }
