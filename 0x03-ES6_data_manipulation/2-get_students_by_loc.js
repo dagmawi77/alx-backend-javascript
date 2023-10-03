8aace73e59960e518551c5c2f338fc5bcf47f491
Dagmawi Letarik <dagmawiletarik@gmail.com>
2023-10-03 11:25:07 +0300

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
     return students.filter((student) => student.location === city);
   }
-  return[];
+  return [];
 }
